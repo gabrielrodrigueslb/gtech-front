c455de545e07168f50330f2b6108763c28d19201
Gabriel <gbri2586@gmail.com>
2025-12-29 14:47:38 -0300

    Fix: Ajuste no icone

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,26 +6,26 @@ import '../styles/globals.scss';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Lintra Tech - Painel',
+  title: 'Lintra_Tech_Painel',
   description: 'Sistema de CRM interno da Lintra Tech',
-  manifest:'/manifest.json',
+  manifest: '/manifest.json',
   icons: {
     icon: [
       { url: '/icon_light.png', media: '(prefers-color-scheme: light)' },
       { url: '/icon.png', media: '(prefers-color-scheme: dark)' },
       { url: '/icon.png', type: 'image/svg+xml' },
     ],
-    apple: '/icon.png',
+    apple: '/apple-icon.png',
   },
 };
 
 export const viewport = {
-  themeColor: "linear-gradient(121.09deg, #11182b 0%, #280f0f 100%)",
-  width:'device-width',
-  initialScale:1,
-  maximumScale:1,
+  themeColor: 'linear-gradient(121.09deg, #11182b 0%, #280f0f 100%)',
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
   userScalable: false,
-}
+};
 
 export default function RootLayout({
   children,
@@ -35,7 +35,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body className={`${inter.className} antialiased`}>
-        <PWARegister/>
+        <PWARegister />
         {children}
       </body>
     </html>
